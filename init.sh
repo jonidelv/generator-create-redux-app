0a0dc03120b02d8e59a7da0a2802771c0754db76
delvallejonatan <delvallejonatan@gmail.com>
2017-03-21 02:07:50 -0300

    Change init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ cp README.md docs/;
 rm README.md;
 ex -sc '1d5|x' ./docs/README.md;
 echo "This project was bootstrapped with[Create Redux App](https://github.com/delvallejonatan/create-redux-app). Refer to **docs** to find information on how to perform common tasks." > README.md;
+npm install;
 rm -rf .git;
 git init && git add . && git commit -m "Initial commit";
-npm install;
 rm init.sh;
