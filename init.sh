12d142d03a40e2f05a20fe26a490b7debc7807b0
delvallejonatan <delvallejonatan@gmail.com>
2017-03-21 02:03:05 -0300

    Delete Bash If Statement

--- init.sh
+++ init.sh
@@ -5,11 +5,7 @@ cp README.md docs/;
 rm README.md;
 ex -sc '1d5|x' ./docs/README.md;
 echo "This project was bootstrapped with[Create Redux App](https://github.com/delvallejonatan/create-redux-app). Refer to **docs** to find information on how to perform common tasks." > README.md;
-rm .git/;
+rm -rf .git;
 git init && git add . && git commit -m "Initial commit";
-if [ yarn --version ]
-then
-  yarn install
-else
-  npm install
-fi
+npm install;
+rm init.sh;
