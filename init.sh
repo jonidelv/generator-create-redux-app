ec18a10d9650b4ba968f26ae51b6cda501156366
delvallejonatan <delvallejonatan@gmail.com>
2017-03-23 00:32:23 -0400

    Fix init script

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rm docs/*
+rm docs/**/*
 cp README.md docs/;
 rm README.md;
 ex -sc '1d5|x' ./docs/README.md;
 echo "This project was bootstrapped with[Create Redux App](https://github.com/delvallejonatan/create-redux-app). Refer to **docs** to find information on how to perform common tasks." > README.md;
 npm install;
 rm -rf .git;
-git init && git add . && git commit -m "Initial commit";
 rm init.sh;
+git init && git add . && git commit -m "Initial commit";
