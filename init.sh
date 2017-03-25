a343474f880c3cd1c3aa82f858757abe70b39997
delvallejonatan <delvallejonatan@gmail.com>
2017-03-25 14:18:32 -0400

    Fix init script

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 rm -rf docs/**;
-cp README.md docs/;
+cp README.md docs/create-redux-app;
 rm README.md;
 ex -sc '1d5|x' ./docs/README.md;
-echo "This project was bootstrapped with[Create Redux App](https://github.com/delvallejonatan/create-redux-app). Refer to **docs** to find information on how to perform common tasks." > README.md;
+echo "This project was bootstrapped with [Create Redux App](https://github.com/delvallejonatan/create-redux-app). Refer to **docs** to find information on how to perform common tasks." > README.md;
 npm install;
 rm -rf .git;
 rm init.sh;
